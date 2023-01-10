620ad7756405e2507342755694168fa013f23a9c
Jan-Philip Loos <maxdaten@gmail.com>
2023-01-10 22:17:57 +0100

    Configure prettier tailwind

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Dollar Holler shell";
+  description = "Dollar Holler Shell";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
