b66f6ebd57f42dce9ff91b708138b582ee0ec627
Jan-Philip Loos <maxdaten@gmail.com>
2023-01-11 23:17:14 +0100

    Lesson 14: Styling the Invoice List

--- src/routes/(dashboard)/invoices/+page.svelte
+++ src/routes/(dashboard)/invoices/+page.svelte
@@ -1,61 +1,60 @@
-Invoices
+<script>
+  import CircledAmount from '$lib/components/CircledAmount.svelte';
+  import ThreeDots from '$lib/components/Icon/ThreeDots.svelte';
+  import View from '$lib/components/Icon/View.svelte';
+  import Search from '$lib/components/Search.svelte';
+  import Tag from '$lib/components/Tag.svelte';
+</script>
 
-<p>
-  Duis dolore tempor dolore commodo adipisicing deserunt nisi laboris in eiusmod officia
-  exercitation esse culpa. Est anim pariatur do tempor incididunt quis veniam commodo nostrud
-  ullamco ea ex est ipsum. Do commodo sint qui do duis reprehenderit id.
-</p>
+<svelte:head>
+  <title>Invoices | The Dollar Holler</title>
+</svelte:head>
 
-<p>
-  Duis dolore tempor dolore commodo adipisicing deserunt nisi laboris in eiusmod officia
-  exercitation esse culpa. Est anim pariatur do tempor incididunt quis veniam commodo nostrud
-  ullamco ea ex est ipsum. Do commodo sint qui do duis reprehenderit id.
-</p>
+<div class="mb-16 flex items-center justify-between">
+  <!-- search field -->
+  <Search />
+  <!-- new invoice button -->
+  <div>
+    <button
+      class="relative translate-y-0 whitespace-nowrap rounded-lg bg-lavenderIndigo px-10 py-3 font-sans text-xl font-black text-white shadow-colored transition-all hover:-translate-y-2 hover:shadow-coloredHover"
+      >+ Invoice</button
+    >
+  </div>
+</div>
 
-<p>
-  Duis dolore tempor dolore commodo adipisicing deserunt nisi laboris in eiusmod officia
-  exercitation esse culpa. Est anim pariatur do tempor incididunt quis veniam commodo nostrud
-  ullamco ea ex est ipsum. Do commodo sint qui do duis reprehenderit id.
-</p>
+<!-- list of invoices -->
+<div>
+  <!-- header -->
+  <div class="table-header invoice-table text-daisyBush">
+    <h3>Status</h3>
+    <h3>Due Date</h3>
+    <h3>ID</h3>
+    <h3>Client</h3>
+    <h3>Amount</h3>
+    <div />
+    <div />
+  </div>
 
-<p>
-  Duis dolore tempor dolore commodo adipisicing deserunt nisi laboris in eiusmod officia
-  exercitation esse culpa. Est anim pariatur do tempor incididunt quis veniam commodo nostrud
-  ullamco ea ex est ipsum. Do commodo sint qui do duis reprehenderit id.
-</p>
+  <!-- invoices -->
+  <div class="invoice-table items-center rounded-lg bg-white py-6 shadow-tableRow">
+    <div><Tag label="draft" /></div>
+    <div class="text-lg">8/1/2022</div>
+    <div class="text-lg">12345</div>
+    <div class="text-xl font-bold">Compressed.fm</div>
+    <div class="font-mono text-lg font-bold">$504.00</div>
+    <div class="center text-lg">
+      <a href="#" class="text-pastelPurple hover:text-daisyBush"><View /></a>
+    </div>
+    <div class="center text-lg">
+      <button class="text-pastelPurple hover:text-daisyBush"><ThreeDots /></button>
+    </div>
+  </div>
+</div>
 
-<p>
-  Duis dolore tempor dolore commodo adipisicing deserunt nisi laboris in eiusmod officia
-  exercitation esse culpa. Est anim pariatur do tempor incididunt quis veniam commodo nostrud
-  ullamco ea ex est ipsum. Do commodo sint qui do duis reprehenderit id.
-</p>
+<CircledAmount label="Total" amount="$1,144.00" />
 
-<p>
-  Duis dolore tempor dolore commodo adipisicing deserunt nisi laboris in eiusmod officia
-  exercitation esse culpa. Est anim pariatur do tempor incididunt quis veniam commodo nostrud
-  ullamco ea ex est ipsum. Do commodo sint qui do duis reprehenderit id.
-</p>
-
-<p>
-  Duis dolore tempor dolore commodo adipisicing deserunt nisi laboris in eiusmod officia
-  exercitation esse culpa. Est anim pariatur do tempor incididunt quis veniam commodo nostrud
-  ullamco ea ex est ipsum. Do commodo sint qui do duis reprehenderit id.
-</p>
-
-<p>
-  Duis dolore tempor dolore commodo adipisicing deserunt nisi laboris in eiusmod officia
-  exercitation esse culpa. Est anim pariatur do tempor incididunt quis veniam commodo nostrud
-  ullamco ea ex est ipsum. Do commodo sint qui do duis reprehenderit id.
-</p>
-
-<p>
-  Duis dolore tempor dolore commodo adipisicing deserunt nisi laboris in eiusmod officia
-  exercitation esse culpa. Est anim pariatur do tempor incididunt quis veniam commodo nostrud
-  ullamco ea ex est ipsum. Do commodo sint qui do duis reprehenderit id.
-</p>
-
-<p>
-  Duis dolore tempor dolore commodo adipisicing deserunt nisi laboris in eiusmod officia
-  exercitation esse culpa. Est anim pariatur do tempor incididunt quis veniam commodo nostrud
-  ullamco ea ex est ipsum. Do commodo sint qui do duis reprehenderit id.
-</p>
+<style lang="postcss">
+  .table-header h3 {
+    @apply text-xl font-black leading-snug;
+  }
+</style>
