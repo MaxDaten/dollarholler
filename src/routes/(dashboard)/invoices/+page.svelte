b9de0d84a285c88453fa515f4251c0d15aeef033
Jan-Philip Loos <maxdaten@gmail.com>
2023-01-11 15:02:27 +0100

    Lesson 13: Responsive Pass on the Dashboard Layout

--- src/routes/(dashboard)/invoices/+page.svelte
+++ src/routes/(dashboard)/invoices/+page.svelte
@@ -1,1 +1,61 @@
 Invoices
+
+<p>
+  Duis dolore tempor dolore commodo adipisicing deserunt nisi laboris in eiusmod officia
+  exercitation esse culpa. Est anim pariatur do tempor incididunt quis veniam commodo nostrud
+  ullamco ea ex est ipsum. Do commodo sint qui do duis reprehenderit id.
+</p>
+
+<p>
+  Duis dolore tempor dolore commodo adipisicing deserunt nisi laboris in eiusmod officia
+  exercitation esse culpa. Est anim pariatur do tempor incididunt quis veniam commodo nostrud
+  ullamco ea ex est ipsum. Do commodo sint qui do duis reprehenderit id.
+</p>
+
+<p>
+  Duis dolore tempor dolore commodo adipisicing deserunt nisi laboris in eiusmod officia
+  exercitation esse culpa. Est anim pariatur do tempor incididunt quis veniam commodo nostrud
+  ullamco ea ex est ipsum. Do commodo sint qui do duis reprehenderit id.
+</p>
+
+<p>
+  Duis dolore tempor dolore commodo adipisicing deserunt nisi laboris in eiusmod officia
+  exercitation esse culpa. Est anim pariatur do tempor incididunt quis veniam commodo nostrud
+  ullamco ea ex est ipsum. Do commodo sint qui do duis reprehenderit id.
+</p>
+
+<p>
+  Duis dolore tempor dolore commodo adipisicing deserunt nisi laboris in eiusmod officia
+  exercitation esse culpa. Est anim pariatur do tempor incididunt quis veniam commodo nostrud
+  ullamco ea ex est ipsum. Do commodo sint qui do duis reprehenderit id.
+</p>
+
+<p>
+  Duis dolore tempor dolore commodo adipisicing deserunt nisi laboris in eiusmod officia
+  exercitation esse culpa. Est anim pariatur do tempor incididunt quis veniam commodo nostrud
+  ullamco ea ex est ipsum. Do commodo sint qui do duis reprehenderit id.
+</p>
+
+<p>
+  Duis dolore tempor dolore commodo adipisicing deserunt nisi laboris in eiusmod officia
+  exercitation esse culpa. Est anim pariatur do tempor incididunt quis veniam commodo nostrud
+  ullamco ea ex est ipsum. Do commodo sint qui do duis reprehenderit id.
+</p>
+
+<p>
+  Duis dolore tempor dolore commodo adipisicing deserunt nisi laboris in eiusmod officia
+  exercitation esse culpa. Est anim pariatur do tempor incididunt quis veniam commodo nostrud
+  ullamco ea ex est ipsum. Do commodo sint qui do duis reprehenderit id.
+</p>
+
+<p>
+  Duis dolore tempor dolore commodo adipisicing deserunt nisi laboris in eiusmod officia
+  exercitation esse culpa. Est anim pariatur do tempor incididunt quis veniam commodo nostrud
+  ullamco ea ex est ipsum. Do commodo sint qui do duis reprehenderit id.
+</p>
+
+<p>
+  Duis dolore tempor dolore commodo adipisicing deserunt nisi laboris in eiusmod officia
+  exercitation esse culpa. Est anim pariatur do tempor incididunt quis veniam commodo nostrud
+  ullamco ea ex est ipsum. Do commodo sint qui do duis reprehenderit id.
+</p>
